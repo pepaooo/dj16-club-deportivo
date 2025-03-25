ad0c47d215605de81e63570b2c0dcee6b862d22b
Jose Angulo <pepaoo@hotmail.com>
2025-03-25 05:59:17 -0600

    initialize project with Maven and Spring Boot setup

---
+++ src/main/java/com/sgdc/core/Dj16SgdcCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sgdc.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dj16SgdcCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dj16SgdcCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgdc/core/Dj16SgdcCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgdc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dj16SgdcCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
