cea1d61fac9b7823e4c7bb28a1546ff890d2410c
pepaoo <pepaoo@hotmail.com>
2025-05-01 07:59:42 -0600

    feat: implement login functionality with success and failure handlers

--- src/main/java/com/sgdc/core/controller/InicioController.java
+++ src/main/java/com/sgdc/core/controller/InicioController.java
@@ -3,6 +3,7 @@ package com.sgdc.core.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class InicioController {
@@ -11,6 +12,23 @@ public class InicioController {
     public String inicio(Model model) {
         model.addAttribute("activePage", "/");
         return "inicio";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login_success_handler")
+    public String loginSuccessHandler() {
+        System.out.println("Logging user login success...");
+        return "inicio";
+    }
+
+    @PostMapping("/login_failure_handler")
+    public String loginFailureHandler() {
+        System.out.println("Login failure handler....");
+        return "login";
     }
 
     @GetMapping("reportes")
