9375c5227f44f7115230315db1267a002763a6d6
pepaoo <pepaoo@hotmail.com>
2025-05-01 15:01:04 -0600

    feat: enhance security configuration with CSRF and CORS support

--- src/main/java/com/sgdc/core/security/SecurityConfiguration.java
+++ src/main/java/com/sgdc/core/security/SecurityConfiguration.java
@@ -6,16 +6,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.SecureRandom;
@@ -70,7 +67,9 @@ public class SecurityConfiguration {
                         //.logoutUrl("/logout")
                         .logoutSuccessUrl("/")
                         .deleteCookies("JSESSIONID") //NEW Cookies to clear
-                        .invalidateHttpSession(true)); //new
+                        .invalidateHttpSession(true))
+                .csrf(Customizer.withDefaults())
+                .cors(Customizer.withDefaults()); //new
         return http.build();
     }
 
