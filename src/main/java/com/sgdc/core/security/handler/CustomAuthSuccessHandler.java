925819ea966f376923c71fb1085923fcbdbb146a
pepaoo <pepaoo@hotmail.com>
2025-05-11 12:49:51 -0600

    feat: update JWT cookie handling to use ResponseCookie for improved security and configuration

--- src/main/java/com/sgdc/core/security/handler/CustomAuthSuccessHandler.java
+++ src/main/java/com/sgdc/core/security/handler/CustomAuthSuccessHandler.java
@@ -4,16 +4,18 @@ import com.sgdc.core.security.jwt.JWTTokenProvider;
 import com.sgdc.core.security.service.LoginAttemptService;
 import com.sgdc.core.usuarios.domain.dto.UsuarioDTO;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.Duration;
 
 @Component
 public class CustomAuthSuccessHandler
@@ -45,12 +47,20 @@ public class CustomAuthSuccessHandler
         // Generar el token JWT
         String jwtToken = jwtTokenProvider.generateJwtToken(authentication, usuarioDTO);
         // Guardar el token en una cookie
-        Cookie jwtCookie = new Cookie("token",jwtToken);
-        jwtCookie.setMaxAge(Integer.MAX_VALUE);
-        jwtCookie.setHttpOnly(true);
-        jwtCookie.setSecure(true);
-        jwtCookie.setPath("/");
-        response.addCookie(jwtCookie);
+//        Cookie jwtCookie = new Cookie("token",jwtToken);
+//        jwtCookie.setMaxAge(Duration.ofHours(1));
+//        jwtCookie.setHttpOnly(true);
+//        jwtCookie.setSecure(true);
+//        jwtCookie.setPath("/");
+//        response.addCookie(jwtCookie);
+        ResponseCookie jwtCookie = ResponseCookie.from("token", jwtToken)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("Strict")
+                .path("/")
+                .maxAge(Duration.ofHours(1))
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
         log.info("JWT token generated and added to cookie");
 
         // Luego redirigir a la URL guardada o al defaultTargetUrl
